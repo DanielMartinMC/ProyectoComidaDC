73220ef728e3a7eecba69f6587961483e19df26d
Carlos <carlosvelazquezcuevas2005@gmail.com>
2026-05-18 00:30:08 +0200

    Arregelando admin pedidos  17/05/2026

--- src/main/java/es/pcomida/proyectocomida/pedido/dto/PedidoUpdateDto.java
+++ src/main/java/es/pcomida/proyectocomida/pedido/dto/PedidoUpdateDto.java
@@ -6,11 +6,17 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.Date;
+
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class PedidoUpdateDto {
+
+    private Long id;
+    private Date fechaPedido;
     private Estado estado;
+    private Double total;
     private String direccion;
 }
