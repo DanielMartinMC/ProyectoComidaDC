c8a23e643173cc6bbf8749ea27a746084952544d
Carlos <carlosvelazquezcuevas2005@gmail.com>
2026-05-18 00:09:36 +0200

    Arregelando PlatoCreateDto  17/05/2026

--- src/main/java/es/pcomida/proyectocomida/plato/dto/PlatoCreateDto.java
+++ src/main/java/es/pcomida/proyectocomida/plato/dto/PlatoCreateDto.java
@@ -8,11 +8,15 @@ import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Builder
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class PlatoCreateDto {
 
     @NotBlank(message = "El nombre no puede estar vacío")
